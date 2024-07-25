4cc1d1e25d36acdd51e05b6f24f2b81200554c66
mymom0937 <mymom0937@gmail.com>
2024-07-25 19:32:29 +0100

    deploying to netlify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from "react";
+import Routing from "./Router.jsx";
+import { auth } from "./Utility/firebase.js";
+import { Type } from "./Utility/action.type.js";
+import { DataContext } from "./Components/DataProvider/DataProvider.jsx";
 
 function App() {
+  const [{ user, basket }, dispatch] = useContext(DataContext);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: Type.SET_USER,
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: Type.SET_USER,
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  // *******
+
+// useEffect(() => {
+//   const unsubscribe = auth.onAuthStateChanged((authUser) => {
+//     if (authUser) {
+//       const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
+//       dispatch({
+//         type: Type.ADD_TO_BASKET,
+//         basket: storedCart,
+//       });
+//     } else {
+//       localStorage.removeItem("cart");
+//       dispatch({
+//         type: Type.ADD_TO_BASKET,
+//         basket: [],
+//       });
+//     }
+//   });
+//   return () => unsubscribe();
+// }, []);
+
+  // *****
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routing />
+    </>
   );
 }
 
